23dc0aab55d773bf876716e4e5a65f75d2e31eca
CGelet <iluvdrpepper03@gmail.com>
2023-04-06 14:31:45 -0400

    Sensor Functions

--- MotorRun.ino
+++ MotorRun.ino
@@ -9,17 +9,22 @@ const int trigPin = ; // USS Trig Pin for all Sensors
 const int echoPin1 = ; // USS recvPin 1
 const int echoPin2 = ; // USS recvPin 2
 const int echoPin3 = ; // USS recvPin 3
-int distance; 
-long duration; 
+int distance1;
+int distance2;
+int distance3; 
+long duration1;
+long duration2;
+long duration3;
 
-const int irRecvPin = ;// IR Recieve Pin
+
+const int irRecvPin = ; // IR Recieve Pin
 int cmd;
 
-const int motorLPin = ;// Left Motor Pin
-const int motorLRev = ;// Left Motor Reverse Pin
+const int motorLPin = ; // Left Motor Pin
+const int motorLRev = ; // Left Motor Reverse Pin
 
-const int motorRPin = ;// Right Motor Pin
-const int motorRRev = ;// Right Motor Reverse Pin
+const int motorRPin = ; // Right Motor Pin
+const int motorRRev = ; // Right Motor Reverse Pin
 
 void setup() {
     serial.begin(9600); // Serial Readout is 9600
@@ -35,11 +40,39 @@ void setup() {
 
     IrReceiver.begin(irRecvPin); // Receiver engables
 
+    digitalWrite(trigPin, LOW); // Find distances at start
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration1 = pulseIn(echoPin1, HIGH);
+    duration2 = pulseIn(echoPin2, HIGH);
+    duration3 = pulseIn(echoPin3, HIGH);
+    distance1f = duration1 * 0.034 / 2;
+    distance2f = duration2 * 0.034 / 2;
+    distance3f = duration3 * 0.034 / 2; // End of Distances at Start
 
     lcd.print('Strt Cmplt');
 }
 
-void sensRead(){
+void sensRead(){ // Sensor reading
+
+digitalWrite(trigPin, LOW); 
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration1 = pulseIn(echoPin1, HIGH);
+duration2 = pulseIn(echoPin2, HIGH);
+duration3 = pulseIn(echoPin3, HIGH);
+
+distance1i = distance1f;
+distance2i = distance2f;
+distance3i = distance3f;
+
+distance1f = duration1 * 0.034 / 2;
+distance2f = duration2 * 0.034 / 2;
+distance3f = duration3 * 0.034 / 2;
 
 }
 
